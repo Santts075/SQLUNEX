691d27b1cbafe6c9ebb798c268af6c01581e96db
Santts075 <danilocnv@gmail.com>
2026-02-11 21:17:21 -0300

    Add files via upload

---
+++ Script-aula03.sql
@@ -0,0 +1,32 @@
+-- insert into cliente (id, nome, cpf)
+-- values (1, 'Danilo', '08404000000')
+
+-- update cliente set nome = 'Joao' where id = 1
+
+-- insert into produto (id, descricao, cod_barras, valor, situacao)
+-- values (2, 'CamisaBrancaG', 01001, 2, true)
+
+-- delete from produto where id = 1
+
+-- select * from produto
+
+-- select * from PRODUTO where id = 1
+
+-- PARA DELETAR E INSERIR NOVOS PRODUTOS DE UMA UNICA VEZ,
+-- ADICIONE ";" NO FINAL E ESCREVA OUTROS PRODUTOS
+
+-- delete from produto;
+-- insert into produto (id, descricao, cod_barras, valor, situacao )
+-- values (1, 'Arroz', '8721398123', 10.20, true);
+-- insert into produto (id, descricao, cod_barras, valor, situacao )
+-- values (2, 'Cafe', '8721398123', 10.20, true);
+-- insert into produto (id, descricao, cod_barras, valor, situacao )
+-- values (3, 'Macarrao', '8721398123', 10.20, true);
+
+--
+
+-- select
+	--id, descricao
+-- from
+	-- produto
+-- order by descricao desc
